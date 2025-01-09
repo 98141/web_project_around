7f208dbbf330d75ec341c123056334231e55ece9
Armando Mora <armandomora14115@gmail.com>
2025-01-09 09:52:00 -0500

    Correciones de ortografía y desbordamientos

--- scripts/index.js
+++ scripts/index.js
@@ -2,23 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const editButton = document.querySelector(".profile__info-button");
   const modal = document.getElementById("editModal");
   const nameInput = document.getElementById("nameInput");
-  const funtionInput = document.getElementById("funtionInput");
+  const functionInput = document.getElementById("functionInput");
   const saveButton = document.getElementById("saveButton");
   const closeButton = document.getElementById("closeButton");
 
   const nameElement = document.querySelector(".profile__info-name");
-  const funtionElement = document.querySelector(".profile__info-funtion");
+  const functionElement = document.querySelector(".profile__info-function");
 
   editButton.addEventListener("click", (event) => {
     event.preventDefault();
     nameInput.value = nameElement.textContent;
-    funtionInput.value = funtionElement.textContent;
+    functionInput.value = functionElement.textContent;
     modal.style.display = "flex";
   });
 
   saveButton.addEventListener("click", () => {
     nameElement.textContent = nameInput.value;
-    funtionElement.textContent = funtionInput.value;
+    functionElement.textContent = functionInput.value;
     modal.style.display = "none";
   });
 
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
     modal.style.display = "none";
   });
 
- 
+
   modal.addEventListener("click", (event) => {
     if (event.target === modal) {
       modal.style.display = "none";
