1730423ba25852ed580944c73cd0147a156a501d
Armando Mora <armandomora14115@gmail.com>
2025-02-15 12:19:56 -0500

    Validaciones completas

--- scripts/index.js
+++ scripts/index.js
@@ -224,6 +224,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.addEventListener("keydown", closeOnEsc);
 
+  /*validadicon perfil*/
   const showInputError = (formElement, inputElement, errorMessage) => {
     const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
     inputElement.classList.add("form__input_type_error");
@@ -286,5 +287,68 @@ const enableValidation = () => {
 };
 
 enableValidation();
+
+/*Validacion de imagenes*/
+const showInputErrorImg = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add("form__input_type_error");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("form__input-error_active");
+};
+
+const hideInputErrorImg = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove("form__input_type_error");
+  errorElement.classList.remove("form__input-error_active");
+  errorElement.textContent = "";
+};
+
+const checkInputValidityImg = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputErrorImg(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputErrorImg(formElement, inputElement);
+  }
+};
+
+const hasInvalidInputImg = (inputList) => {
+  return inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonStateImg = (inputList, buttonElement) => {
+  if (hasInvalidInputImg(inputList)) {
+      buttonElement.classList.add("button_inactive");
+  } else {
+      buttonElement.classList.remove("button_inactive");
+      buttonElement.disabled = false;
+  }
+};
+
+const setEventListenersImg = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(".form__input-img"));
+  const buttonElement = formElement.querySelector(".form__submit");
+
+  toggleButtonStateImg(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", function () {
+        checkInputValidityImg(formElement, inputElement);
+          toggleButtonStateImg(inputList, buttonElement);
+      });
+  });
+};
+
+const enableValidationImg = () => {
+  const formElement = document.querySelector('form[name="formImg"]');
+  formElement.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+  });
+
+  setEventListenersImg(formElement);
+};
+
+enableValidationImg();
 
 });
